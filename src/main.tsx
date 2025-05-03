afab1f1fe9010867e6e8ed085e67a2698914a394
laurent loukopoulos <ll@awesomelab.io>
2025-05-03 14:23:20 +0200

    feat: initialize React application with Vite and TypeScript
    - Added pnpm workspace configuration to only build dependencies for esbuild.
    - Created main application component (App) and rendered it in main.tsx.
    - Created Vite configuration file with React and React Scan plugins.

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { scan } from "react-scan";
+scan({ enabled: import.meta.env.DEV });
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import reactScan from "@react-scan/vite-plugin-react-scan";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    reactScan({
+      enable: process.env.REACT_APP_SCAN === "enabled",
+    }),
+  ],
+});
