ee3a9250a086d38095522c172487a81594e98a2b
laurent loukopoulos <ll@awesomelab.io>
2025-05-03 17:43:01 +0200

    style: update StyledInput to use consistent border-radius and background color

--- src/component/styles/Input.styled.tsx
+++ src/component/styles/Input.styled.tsx
@@ -5,8 +5,8 @@ export const StyledInput = styled.input`
   padding: 0.75rem 1rem;
   margin-bottom: 2rem;
   border: 1px solid ${(props) => props.theme.colors.border};
-  border-radius: 0.5rem;
-  background-color: ${(props) => props.theme.colors.cardBackground};
+  border-radius: 25px;
+  background-color: ${(props) => props.theme.colors.searchbarBackground};
   color: ${(props) => props.theme.colors.text};
   font-size: 1rem;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
